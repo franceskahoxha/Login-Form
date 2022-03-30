e2cdb8da2bde6aca99734fc4837d40bea727f38f
franceska <franceskahoxha7@gmail.com>
2022-03-30 20:20:44 +0200

    login-form

--- modal.js
+++ modal.js
@@ -5,3 +5,11 @@ function UnSelectAll(){
       items[i].checked=false;
   }
 }
+
+function SelectAll(){
+  var items=document.getElementsByName('checkbox');
+  for(var i=0; i<items.length; i++){
+    if(items[i].type=='checkbox')
+      items[i].checked=true;
+  }
+}
